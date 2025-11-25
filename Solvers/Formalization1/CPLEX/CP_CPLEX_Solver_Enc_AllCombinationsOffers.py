a9832bd93d867b5399edf5939bd1328a9291f95e
alexciuras <alexciuras@gmail.com>
2025-11-25 14:47:15 +0200

    Cplex modification

--- Solvers/Formalization1/CPLEX/CP_CPLEX_Solver_Enc_AllCombinationsOffers.py
+++ Solvers/Formalization1/CPLEX/CP_CPLEX_Solver_Enc_AllCombinationsOffers.py
@@ -145,4 +145,3 @@ class CPlex_Solver_SB_Enc_AllCombinationsOffers(CPlex_Solver_Parent, ManuverSolv
         for index, var in self.vmType.items():
             vm_types.append(var.solution_value)
         return vm_types
-#
